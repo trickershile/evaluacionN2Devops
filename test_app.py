9d8d44bbe93b0adf4432a865da4db78f1cbf6df3
Trickershile <vict.garay@duocuc.cl>
2026-05-26 23:05:07 -0400

    Fix formatting and add newline at end of file in test cases

--- test_app.py
+++ test_app.py
@@ -24,14 +24,16 @@ def test_create_producto(client):
 
 def test_create_producto_invalido(client):
     # Prueba de manejo de error 400
-    rv = client.post('/productos', json={"nombre": "Galletas"}) # Falta el precio
+    rv = client.post('/productos', json={"nombre": "Galletas"})  # Falta el precio
     assert rv.status_code == 400
     assert b'Faltan datos requeridos' in rv.data
+
 
 def test_update_producto(client):
     rv = client.put('/productos/1', json={"precio": 16000})
     assert rv.status_code == 200
     assert b'16000' in rv.data
+
 
 def test_delete_producto_no_existente(client):
     # Prueba de manejo de error 404
